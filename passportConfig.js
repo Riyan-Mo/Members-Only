ba6fa51c5999a8555d510122bf519199c5493eae
Riyan-Mo <riyan8308151899@gmail.com>
2024-07-06 08:02:47 +0530

    Creates passport config file

--- passportConfig.js
+++ passportConfig.js
@@ -1,6 +1,42 @@
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
 
-function passportInit(passport) {}
+function passportInit() {
+  passport.use(
+    new LocalStrategy(async function (username, password, done) {
+      try {
+        const user = await User.findOne({ username: username });
+        if (!user) {
+          return done(null, false, {
+            message: "User does not exist in database.",
+          });
+        }
+        const isPasswordCorrect = bcrypt.compare(password, user.passwordHash);
+        if (isPasswordCorrect) {
+          return done(null, user);
+        }
+      } catch (e) {
+        return done(null, false, { message: "Username is not valid." });
+      }
+    })
+  );
+
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+
+  return passport;
+}
 
 module.exports = passportInit;
