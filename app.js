d36505037620690fd55ca2914a935b091542407e
Riyan-Mo <riyan8308151899@gmail.com>
2024-07-06 08:01:16 +0530

    Changes app to use controllers

--- app.js
+++ app.js
@@ -1,113 +1,60 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const bcrypt = require("bcryptjs");
-const connectMongo = require("connect-mongoose");
+const MongoStore = require("connect-mongo");
+const passportInit = require("./passportConfig");
+const signupController = require("./controllers/signupController");
+const loginController = require("./controllers/loginController");
+const homeController = require("./controllers/homeController");
+const { isAuth, isLoggedIn } = require("./middlewares/authMiddleware");
 
-const mongoDb = "";
+const passport = passportInit();
+const mongoDb = process.env.MONGODB_URI;
 mongoose.connect(mongoDb);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
-const User = mongoose.model(
-  "User",
-  new Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true }
-  })
-);
-
 const app = express();
-app.set("views", __dirname+"/views");
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(
+  session({
+    secret: "cats",
+    resave: false,
+    saveUninitialized: true,
+    // store: MongoStore.create({ mongoUrl: mongoDb }),
+  })
+);
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await User.findOne({ username: username });
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      };
-      if (user.password !== password) {
-        return done(null, false, { message: "Incorrect password" });
-      };
-      return done(null, user);
-    } catch(err) {
-      return done(err);
-    };
-  })
-);
+app.get("/", isLoggedIn, (req, res) => res.render("index"));
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
+app.get("/home", isAuth, homeController.get);
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch(err) {
-    done(err);
-  };
-});
+app.get("/signup", isLoggedIn, signupController.get);
 
-app.get("/", (req, res) => res.render("index", {user: req.user}));
+app.post("/signup", signupController.post);
 
-app.get("/sign-up", (req, res)=>{
-  res.render("sign-up-form")  
-})
-
-app.post("/sign-up", (req, res, next)=>{
-  bcrypt.hash(req.body.password, 10, async(err, hashedPassword)=>{
-    if(err){
-      res.render("sign-up-form")
-      next();
-      }
-      else{
-      try{
-        const user = new User({
-          username: req.body.username,
-          password: hashedPassword,
-        })
-        await user.save();
-        res.redirect("/");
-      }catch(e){
-        return next(e);
-      }
-    }
-  })
-})
-
-app.get("/login", (req, res)=>res.render("login-in-form"))
-
-app.get("/log-out", (req, res, next)=>{
-  req.logout((err)=>{
-    if(err){
-      return next(err);
-    }
-    res.redirect("/");
-  })
-})
+app.get("/login", isLoggedIn, loginController.get);
 
 app.post(
-  "/log-in",
+  "/login",
+  loginController.post,
   passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login"
+    successRedirect: "/home",
+    failureRedirect: "/login",
   })
 );
 
-app.use((err, req, res, next)=>{
-  console.log(err)
-  res.redirect("/")
-})
+app.get("/logout", (req, res, next) => {
+  req.logOut((err) => {
+    console.log(err);
+    return res.redirect("/login");
+  });
+});
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
