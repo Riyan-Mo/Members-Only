fb50e44a34f4856230963340da5d9b922bbb0f4e
Riyan-Mo <riyan8308151899@gmail.com>
2024-07-07 18:16:34 +0530

    Adds validation for sending messages

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,5 +1,6 @@
 const Message = require("../models/Message");
 const User = require("../models/User");
+const { validationResult, body } = require("express-validator");
 
 exports.getMessages = async (req, res) => {
   const messages = await Message.find().populate("user").exec();
@@ -10,17 +11,27 @@ function timeStampExtractor(Date) {
   return `${Date.toLocaleDateString()} ${Date.toLocaleTimeString()}`;
 }
 
-exports.postMessage = async (req, res) => {
-  const { message, title } = req.body;
-  const { _id } = req.user;
-  const timeStamp = timeStampExtractor(new Date());
-  try {
-    await new Message({ user: _id, message, title, timeStamp }).save();
-  } catch (e) {
-    console.log(e);
-  }
-  res.redirect("/");
-};
+exports.postMessage = [
+  body("title", "Title is too short").exists().isString({ min: 2 }).escape(),
+  body("message", "Message is too short")
+    .exists()
+    .isString({ min: 2 })
+    .escape(),
+  async (req, res) => {
+    const { errors } = validationResult(req);
+    if (errors.length === 0) {
+      const { message, title } = req.body;
+      const { _id } = req.user;
+      const timeStamp = timeStampExtractor(new Date());
+      try {
+        await new Message({ user: _id, message, title, timeStamp }).save();
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    return res.redirect("/");
+  },
+];
 
 exports.deleteMessage = async (req, res) => {
   const { id } = req.body;
